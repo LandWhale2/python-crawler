9cceebe08168401eed63921697b438e36cda1140
kcuyh <khj879123@gmail.com>
2020-01-21 20:39:34 +0900

    [update] webtoon crawler

--- naverwebtoonCrawler.py
+++ naverwebtoonCrawler.py
@@ -13,7 +13,15 @@ def get_html(url):
 url = "http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=tue&page=1"
 html = get_html(url)
 soup = BeautifulSoup(html, 'lxml')
-l = soup.find_all("a")
-print(len(l))
-
-
+webtoon_list = list()
+webtoon_area = soup.find("table", {"class" : "viewList"}).find_all("td", {"class" : "title"})
+for webtoon_index in webtoon_area:
+    info_soup = webtoon_index.find("a")
+    _url = info_soup["href"]
+    _text = info_soup.text.split(".")
+    _title = ""
+    _num = _text[0]
+    if len(_text) > 1:
+        _title = _text[1]
+    webtoon_list.append((_num, _title, _url, ))
+print(webtoon_list)
