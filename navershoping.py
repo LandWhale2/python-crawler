707b090fb11ac8699fd93760718ef0d57d1f683f
kcuyh <khj879123@gmail.com>
2020-02-02 10:48:32 +0900

    etv

--- navershoping.py
+++ navershoping.py
@@ -1,8 +1,8 @@
 import bs4
 import urllib.request
 
-url = "https://shopping.naver.com/living/homeliving/category?menu=10000921"
+url = "https://swindow.naver.com/designer/list/category"
 html = urllib.request.urlopen(url)
 soup = bs4.BeautifulSoup(html, "html.parser")
-items = soup.find("a", {"class":"_8tp7k5rNMY"})
+items = soup.select('div >div > ul > li')
 print(items)
