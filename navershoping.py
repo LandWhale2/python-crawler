33d6d144467e28b6fcd623234c29d7599b0043cc
kcuyh <khj879123@gmail.com>
2020-02-06 12:15:27 +0900

    etc

--- navershoping.py
+++ navershoping.py
@@ -4,7 +4,7 @@ import urllib.request
 url = "https://swindow.naver.com/designer/list/category"
 html = urllib.request.urlopen(url)
 soup = bs4.BeautifulSoup(html, "html.parser")
-# items = soup.find_all('li', {"class": "item  _NEW_ITEM_LAYOUT"})
+items = soup.find_all('li', {"class": "item  _NEW_ITEM_LAYOUT"})
 items = soup
 print(items)
 
