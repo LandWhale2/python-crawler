bdb6940afbeed03c0ef20eda11b82be069e58d1f
kcuyh <khj879123@gmail.com>
2020-02-09 12:02:10 +0900

    naver shoping

--- navershoping.py
+++ navershoping.py
@@ -4,9 +4,9 @@ import urllib.request
 url = "https://search.shopping.naver.com/search/all.nhn?query=%EB%84%A4%EC%9D%B4%EB%B2%84+%EC%87%BC%ED%95%91&frm=NVSCPRO"
 html = urllib.request.urlopen(url)
 soup = bs4.BeautifulSoup(html, "html.parser")
-items = soup.find('ul', {"class": "goods_list"}).find_all('ul', {"class": "goods_list"})
+items = soup.find_all('li', {"class": "_itemSection"})
 for item in items:
-    tmp = item.find('div', {"class": "info"})
-    print(tmp)
+    tmp = item.find('div', {"class": "tit"})
+    print(tmp.text)
 
 
