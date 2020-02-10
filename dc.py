2dcfaba73319337dc3f2d3bc15f681140ff32ccf
kcuyh <khj879123@gmail.com>
2020-02-10 12:03:24 +0900

    dc

---
+++ dc.py
@@ -0,0 +1,26 @@
+import bs4
+import requests
+
+
+def get_html():
+   url = 'https://gall.dcinside.com/mgallery/board/lists/'
+   payload = {"id": "leemalnyeon", "page" : "1"}
+   headers = {
+       "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+       "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+   }
+   r = requests.get(url, params=payload, headers = headers)
+   
+   if r.status_code == 200:
+      _html = r.text
+   return _html
+
+
+
+
+html = get_html()
+soup = bs4.BeautifulSoup(html, "lxml")
+print(soup)
+
+items = soup.find('tr', {"class" : "ub-content us-post"})
+print(items)
--- navershoping.py
+++ navershoping.py
@@ -3,10 +3,8 @@ import urllib.request
 
 url = "https://search.shopping.naver.com/search/all.nhn?query=%EB%84%A4%EC%9D%B4%EB%B2%84+%EC%87%BC%ED%95%91&frm=NVSCPRO"
 html = urllib.request.urlopen(url)
-soup = bs4.BeautifulSoup(html, "html.parser")
+soup = bs4.BeautifulSoup(html, "lxml")
 items = soup.find_all('li', {"class": "_itemSection"})
 for item in items:
     tmp = item.find('div', {"class": "tit"})
     print(tmp.text)
-
-
