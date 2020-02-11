774ae44106335cfa70b12eb8d7a90c99ff979e2a
kcuyh <khj879123@gmail.com>
2020-02-11 11:57:15 +0900

    dc

--- dc.py
+++ dc.py
@@ -4,7 +4,7 @@ import requests
 
 def get_html():
    url = 'https://gall.dcinside.com/mgallery/board/lists/'
-   payload = {"id": "leemalnyeon", "page" : "1"}
+   payload = {'id': 'leemalnyeon', 'page' : '1'}
    headers = {
        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
@@ -20,7 +20,13 @@ def get_html():
 
 html = get_html()
 soup = bs4.BeautifulSoup(html, "lxml")
-print(soup)
-
-items = soup.find('tr', {"class" : "ub-content us-post"})
-print(items)
+items = soup.find_all('tr')
+for item in items:
+   content = item.find('a')
+   if content != None:
+      print(content.text)
+   
+# print(soup)
+# print(items)
+# items = soup.find('tr', {"class" : "ub-content us-post"})
+# print(items)
